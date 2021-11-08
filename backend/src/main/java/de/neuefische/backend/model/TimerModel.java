fecdaec309a847f8ef70bf9a0903a20bcc76fc25
Max Lenz <maxlenz@mail.de>
2021-11-08 14:07:26 +0100

    Replace custom duration attribute with java duration class

--- backend/src/main/java/de/neuefische/backend/model/TimerModel.java
+++ backend/src/main/java/de/neuefische/backend/model/TimerModel.java
@@ -3,10 +3,12 @@ package de.neuefische.backend.model;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.Duration;
+
 @Data
 @NoArgsConstructor
 public class TimerModel {
 
-    private String timerDuration;
+    private Duration duration;
     private String id;
 }
