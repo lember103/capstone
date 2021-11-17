de9663adabe695340714cde11ee3b9afdda620b0
Max Lenz <maxlenz@mail.de>
2021-11-17 17:20:12 +0100

    Changes according to review

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,16 @@
 import PumpAction from "./components/PumpAction";
-import styled from "styled-components";
 import WeatherWidget from "./components/WeatherWidget";
 
 function App() {
 
     return (
-        <Styled>
+        <>
             <PumpAction/>
             <WeatherWidget/>
-        </Styled>
+        </>
 
     );
 }
 
 export default App;
 
-const Styled = styled.div `
-
-`
