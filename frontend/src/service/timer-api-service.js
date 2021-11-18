a0173c9b08da9aaf298dd1725e8874a9af8f1491
Max Lenz <maxlenz@mail.de>
2021-11-18 12:55:58 +0100

    Fix token handling

--- frontend/src/service/timer-api-service.js
+++ frontend/src/service/timer-api-service.js
@@ -1,23 +1,32 @@
 import axios from "axios";
 
-export const runPump = minutes => {
+const getHeader = token => {
+    console.log(token)
+    return {
+        headers:{
+            Authorization: `Bearer ${token}`
+        }
+    }
+}
+
+export const runPump = (minutes, token) => {
     const timer = {
         pumpRunTimeInMinutes: minutes,
         stopPump: false
     }
     return axios
-        .post("/api/timer", timer)
+        .post("/api/timer", timer, getHeader(token))
         .then(response => response.data)
         .catch(error => console.log(error))
 }
 
-export const stopPump = minutes => {
+export const stopPump = (minutes, token) => {
     const timer = {
         pumpRunTimeInMinutes: minutes,
         stopPump: true
     }
     return axios
-        .post("/api/timer", timer)
+        .post("/api/timer", timer, getHeader(token))
         .then(response => response.data)
         .catch(error => console.log(error))
 }
