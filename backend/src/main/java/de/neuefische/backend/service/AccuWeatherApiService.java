0ac2b991c483d4ec087638d16c8841e76a8d39eb
Max Lenz <maxlenz@mail.de>
2021-11-19 11:09:40 +0100

    Get apikey with constructor injection

--- backend/src/main/java/de/neuefische/backend/service/AccuWeatherApiService.java
+++ backend/src/main/java/de/neuefische/backend/service/AccuWeatherApiService.java
@@ -2,6 +2,7 @@ package de.neuefische.backend.service;
 
 import de.neuefische.backend.model.DailyForecasts;
 import de.neuefische.backend.model.WeatherForecast;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -12,13 +13,16 @@ import java.util.Objects;
 @Service
 public class AccuWeatherApiService {
 
-    @Value("${microgardener.accuweather.apikey}")
-    private static String API_KEY = "xiXJGJfVofWBtkbSj51vTclQh0ognRyA";
-    private static final String API_URL = "http://dataservice.accuweather.com/forecasts/v1/daily/1day/178087?apikey="+API_KEY+"&details=true&metric=true";
+    private final String apiURL;
     private final RestTemplate restTemplate = new RestTemplate();
 
+    @Autowired
+    public AccuWeatherApiService(@Value("${API_KEY}") String apiKEY) {
+        this.apiURL = "http://dataservice.accuweather.com/forecasts/v1/daily/1day/178087?apikey="+apiKEY+"&details=true&metric=true";
+    }
+
     public DailyForecasts getWeatherForecastFromAccuWeather() {
-        ResponseEntity<WeatherForecast> response = restTemplate.getForEntity(API_URL, WeatherForecast.class);
+        ResponseEntity<WeatherForecast> response = restTemplate.getForEntity(apiURL, WeatherForecast.class);
         return Objects.requireNonNull(response.getBody(), "There were no data received from OpenWeather").getDailyForecasts().get(0);
     }
 
