4ddd13a4adc4350afc4cc18ba7cb533daa6f89cf
Max Lenz <maxlenz@mail.de>
2021-11-21 11:53:12 +0100

    Fix tests which require authentication

--- backend/src/test/java/de/neuefische/backend/controller/TimerControllerTest.java
+++ backend/src/test/java/de/neuefische/backend/controller/TimerControllerTest.java
@@ -1,12 +1,20 @@
 package de.neuefische.backend.controller;
 
 import de.neuefische.backend.model.PumpTimer;
+import de.neuefische.backend.security.model.AppUser;
+import de.neuefische.backend.security.repo.AppUserRepo;
 import de.neuefische.backend.service.TimerService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -19,6 +27,21 @@ class TimerControllerTest {
     @Autowired
     private TimerService timerService;
 
+    @Autowired
+    private AppUserRepo userRepo;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    private HttpHeaders getHttpHeadersWithJWT() {
+        userRepo.save(AppUser.builder().username("test_username").password(passwordEncoder.encode("some-password")).build());
+        AppUser loginData = new AppUser("test_username", "some-password");
+        ResponseEntity<String> response = testRestTemplate.postForEntity("/auth/login", loginData, String.class);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(Objects.requireNonNull(response.getBody()));
+        return headers;
+    }
+
     @Test
     void getTimer() {
         //GIVEN
@@ -30,7 +53,12 @@ class TimerControllerTest {
         timerService.update(timer);
 
         //WHEN
-        ResponseEntity<PumpTimer> getResponse = testRestTemplate.getForEntity("/api/timer", PumpTimer.class);
+        ResponseEntity<PumpTimer> getResponse = testRestTemplate.exchange(
+                "/api/timer",
+                HttpMethod.GET,
+                new HttpEntity<>(getHttpHeadersWithJWT()),
+                PumpTimer.class
+        );
         PumpTimer actual = getResponse.getBody();
 
         //THEN
@@ -46,7 +74,11 @@ class TimerControllerTest {
         timer.setStopPump(false);
 
         //WHEN
-        ResponseEntity<PumpTimer> postResponse = testRestTemplate.postForEntity("/api/timer", timer, PumpTimer.class);
+        ResponseEntity<PumpTimer> postResponse = testRestTemplate.exchange(
+                "/api/timer",
+                HttpMethod.POST,
+                new HttpEntity<>(timer, getHttpHeadersWithJWT()),
+                PumpTimer.class);
         PumpTimer actual = postResponse.getBody();
         assert actual != null;
         timer.setId(actual.getId());
