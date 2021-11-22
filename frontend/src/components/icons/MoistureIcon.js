c950943d3be37dd46884979e378d388471effec9
Max Lenz <maxlenz@mail.de>
2021-11-22 23:45:33 +0100

    Add custom icons

---
+++ frontend/src/components/icons/MoistureIcon.js
@@ -0,0 +1,48 @@
+export default function MoistureIcon(){
+    return <svg width="12.7mm" height="12.7mm" version="1.1" viewBox="0 0 12.7 12.7" xmlns="http://www.w3.org/2000/svg">
+        <defs>
+            <filter id="filter291" x="-.0015767" y="0" width="1.0032" height="1.0019" colorInterpolationFilters="sRGB">
+                <feColorMatrix result="color1" type="hueRotate" values="180"/>
+                <feColorMatrix result="fbSourceGraphic" values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 -0.21 -0.72 -0.07 2 0 "/>
+                <feColorMatrix in="fbSourceGraphic" result="fbSourceGraphicAlpha" values="0 0 0 -1 0 0 0 0 -1 0 0 0 0 -1 0 0 0 0 1 0"/>
+                <feColorMatrix in="fbSourceGraphic" result="color1" type="hueRotate" values="180"/>
+                <feColorMatrix result="fbSourceGraphic" values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 -0.21 -0.72 -0.07 2 0 "/>
+                <feColorMatrix in="fbSourceGraphic" result="fbSourceGraphicAlpha" values="0 0 0 -1 0 0 0 0 -1 0 0 0 0 -1 0 0 0 0 1 0"/>
+                <feColorMatrix in="fbSourceGraphic" result="color1" type="hueRotate" values="180"/>
+                <feColorMatrix result="fbSourceGraphic" values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 -0.21 -0.72 -0.07 2 0 "/>
+                <feColorMatrix in="fbSourceGraphic" result="fbSourceGraphicAlpha" values="0 0 0 -1 0 0 0 0 -1 0 0 0 0 -1 0 0 0 0 1 0"/>
+                <feColorMatrix in="fbSourceGraphic" result="color2" values="-1 0 0 0 1 0 -1 0 0 1 0 0 -1 0 1 -0.21 -0.72 -0.07 2 0 "/>
+            </filter>
+        </defs>
+        <circle cx="6.35" cy="6.35" r="6.35" strokeWidth=".55864"/>
+        <g transform="matrix(1.7363 0 0 1.6673 -8.3022 -88.092)" filter="url(#filter291)">
+            <rect x="7.6976" y="56.172" width=".44993" height="2.7051" rx=".049094" strokeWidth=".0078301"/>
+            <rect x="8.6391" y="56.182" width=".44993" height="2.7051" rx=".049094" strokeWidth=".0078301"/>
+            <rect x="7.7205" y="59.07" width=".41435" height=".80983" rx=".46504" strokeWidth=".0080886"/>
+            <rect x="7.8023" y="59.21" width=".26732" height=".55576" rx=".30003" fill="#fff" strokeWidth=".0053821"/>
+            <rect x="7.719" y="58.946" width=".41435" height=".4633" strokeWidth=".0058232"/>
+            <rect x="7.8021" y="59.093" width=".26418" height=".35606" fill="#fff" strokeWidth=".0040762"/>
+            <rect x="8.6576" y="59.077" width=".41435" height=".80983" rx=".46504" strokeWidth=".0080886"/>
+            <rect x="8.7393" y="59.217" width=".26732" height=".55576" rx=".30003" fill="#fff" strokeWidth=".0053821"/>
+            <rect x="8.6561" y="58.953" width=".41435" height=".4633" strokeWidth=".0058232"/>
+            <rect x="8.7392" y="59.1" width=".26418" height=".35606" fill="#fff" strokeWidth=".0040762"/>
+            <rect x="9.39" y="57.664" width="2.2087" height=".14857" rx=".091175" strokeWidth=".0066739"/>
+            <rect x="5.2487" y="57.705" width="2.2087" height=".14857" rx=".091175" strokeWidth=".0066739"/>
+            <g strokeWidth=".0039011">
+                <rect x="5.5257" y="58.168" width=".75464" height=".14857" rx=".031151"/>
+                <rect x="6.3807" y="58.679" width=".75464" height=".14857" rx=".031151"/>
+                <rect x="9.7305" y="58.068" width=".75464" height=".14857" rx=".031151"/>
+                <rect x="10.54" y="58.663" width=".75464" height=".14857" rx=".031151"/>
+            </g>
+            <rect x="6.9421" y="53.537" width="2.7793" height="1.5517" rx=".6195" strokeWidth=".0069328"/>
+            <rect x="7.0905" y="53.654" width="2.4848" height="1.3206" rx=".55385" fill="#fff" strokeWidth=".0060474"/>
+            <path d="m6.9436 54.577s-0.00874 1.4321 1.3581 1.5133c1.6148 0.09591 1.421-1.5629 1.421-1.5629z" stroke="#000" strokeWidth=".0087866px"/>
+            <path d="m7.0905 54.461s-0.00787 1.4108 1.2225 1.4908c1.4536 0.09448 1.2792-1.5397 1.2792-1.5397z" fill="#fff" stroke="#000" strokeWidth=".0082743px"/>
+            <path transform="scale(-1)" d="m-7.365-54.445a0.96638 0.62728 0 0 1-0.96064 0.62727 0.96638 0.62728 0 0 1-0.97205-0.61982"/>
+            <rect transform="matrix(.8725 -.48861 .62694 .77907 0 0)" x="-28.222" y="51.733" width=".15641" height="1.2016" rx=".1043" fill="#fff" strokeWidth=".0088485"/>
+        </g>
+    </svg>
+
+
+
+}
