09a49d826c763b36972651950bb680e591e13222
Max Lenz <maxlenz@mail.de>
2021-11-22 09:57:35 +0100

    Style homepage layout

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,7 +2,7 @@ import styled from "styled-components/macro";
 
 export default function Header(){
     return(
-        <StyledHeader width="150" height="130">
+        <StyledHeader width="150" height="100">
             <text x="0" y="72" fontSize="30px" fill="#338032" fontFamily="Trebuchet MS">micro</text>
             <text x="0" y="100" fontSize="30px" fontWeight="bold" fill="#338032" fontFamily="Trebuchet MS">GARDENER</text>
             <image x="80" y="25" href="seedling.png" width="70" fill="blue"/>
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,14 +1,23 @@
 import PumpAction from "../components/PumpAction";
 import WeatherWidget from "../components/WeatherWidget";
 import Header from "../components/Header";
+import styled from "styled-components/macro";
 
 export default function Homepage(){
 
     return(
-        <>
+        <PageStyle>
             <Header/>
-            <PumpAction/>
             <WeatherWidget/>
-        </>
+            <PumpAction/>
+        </PageStyle>
     )
 }
+
+const PageStyle = styled.div`
+  display: flex;
+  justify-content: space-between;
+  gap: 20px;
+  align-items: center;
+  flex-direction: column
+`
